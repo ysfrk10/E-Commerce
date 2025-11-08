deae794cf8168d2c6138d20688c709618046118d
Youssef farouk <faroukyousef0@gmail.com>
2025-11-08 22:07:33 +0200

    finish responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ export default function NavBar() {
   return (
     <nav
       className=" z-50 top-0 w-full bg-white/50
-     mt-0 fixed backdrop-blur-md  border-b dark:bg-gray-900/50 
+     mt-0 fixed backdrop-blur-lg  border-b dark:bg-gray-900/50 
      dark:border-b dark:border-gray-700"
     >
       <div
@@ -33,10 +33,10 @@ export default function NavBar() {
         >
           <img
             src="../../../public/Y.png"
-            className="h-9 bg-[black] rounded-md"
+            className="h-9 bg-[black] rounded-lg"
             alt="Flowbite Logo"
           />
-          <span className="hidden md:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="hidden lg:block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Store
           </span>
         </a>
@@ -49,7 +49,7 @@ export default function NavBar() {
             type="button"
             className="items-center p-2 w-10 h-10
              justify-center text-sm
-             text-gray-500  md:hidden 
+             text-gray-500  lg:hidden 
              dark:text-gray-400"
             aria-controls="navbar-default"
             aria-expanded="false"
@@ -76,7 +76,7 @@ export default function NavBar() {
             onClick={() => {
               Navigate("/cart");
             }}
-            className="block md:hidden relative"
+            className="block lg:hidden relative"
           >
             <ShoppingCartOutlinedIcon />
             <span
@@ -90,33 +90,33 @@ export default function NavBar() {
         <div
           className={
             show
-              ? "block w-full md:block md:w-auto"
-              : "hidden w-full md:block md:w-auto"
+              ? "block w-full lg:block lg:w-auto"
+              : "hidden w-full lg:block lg:w-auto"
           }
           id="navbar-default"
         >
           {/*  className="font-medium
-           flex flex-col p-4 md:p-0 mt-4 border 
+           flex flex-col p-4 lg:p-0 mt-4 border 
            border-gray-100 rounded-lg bg-gray-50
-            md:flex-row md:space-x-8 rtl:space-x-reverse 
-            md:mt-0 md:border-0 md:bg-white dark:bg-gray-800
-             md:dark:bg-gray-900 dark:border-gray-700" */}
+            lg:flex-row lg:space-x-8 rtl:space-x-reverse 
+            lg:mt-0 lg:border-0 lg:bg-white dark:bg-gray-800
+             lg:dark:bg-gray-900 dark:border-gray-700" */}
           <ul
             className="font-medium flex
-           flex-col p-4 md:p-0 mt-4 border 
+           flex-col p-4 lg:p-0 mt-4 border 
             rounded-lg 
-            md:flex-row md:space-x-8 rtl:space-x-reverse
-             md:items-center
-            md:mt-0 md:border-0"
+            lg:flex-row lg:space-x-8 rtl:space-x-reverse
+             lg:items-center
+            lg:mt-0 lg:border-0"
           >
             <li>
               <a
                 href="#"
                 className="block py-2 px-3 
                 text-white bg-blue-700 
-                rounded-sm md:bg-transparent
-                md:text-blue-700 md:p-0
-                 dark:text-white md:dark:text-blue-500"
+                rounded-sm lg:bg-transparent
+                lg:text-blue-700 lg:p-0
+                 dark:text-white lg:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
@@ -129,13 +129,13 @@ export default function NavBar() {
                 className="transition duration-300
                   block py-2 px-3 text-gray-900
                    rounded-sm hover:bg-gray-100
-                    md:hover:bg-transparent md:border-0
-                     md:hover:text-blue-700 md:p-0
+                    lg:hover:bg-transparent lg:border-0
+                     lg:hover:text-blue-700 lg:p-0
                       dark:text-white
-                       md:dark:hover:text-blue-500
+                       lg:dark:hover:text-blue-500
                         dark:hover:bg-gray-700 
                         dark:hover:text-white 
-                        md:dark:hover:bg-transparent"
+                        lg:dark:hover:bg-transparent"
               >
                 Products
               </a>
@@ -143,7 +143,7 @@ export default function NavBar() {
             <li>
               <a
                 href="#"
-                className="transition duration-300 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="transition duration-300 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
               >
                 Categories
               </a>
@@ -151,7 +151,7 @@ export default function NavBar() {
             <li>
               <a
                 href="#"
-                className="transition duration-300 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="transition duration-300 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
               >
                 Settings
               </a>
@@ -160,7 +160,7 @@ export default function NavBar() {
               onClick={() => {
                 Navigate("cart");
               }}
-              className=" relative transition duration-300  hidden md:block  py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "
+              className=" relative transition duration-300  hidden lg:block  py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent "
             >
               <ShoppingCartOutlinedIcon />
               <span
@@ -214,7 +214,7 @@ function Search() {
           }}
           type="search"
           id="default-search"
-          className="block w-[full] md:w-150 p-4 ps-10 text-sm
+          className="block w-[full] lg:w-150 p-4 ps-10 text-sm
            text-gray-900 border border-gray-300 rounded-lg
             bg-gray-50 
             dark:bg-gray-700 dark:border-gray-600
